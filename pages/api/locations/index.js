b04ef8aaf1df728ca316ffffb1c8ba62ca8480a4
Juan <pront23@gmail.com>
2021-11-03 15:15:54 +0900

    added find all function

---
+++ pages/api/locations/index.js
@@ -0,0 +1,14 @@
+import nc from 'next-connect';
+import db from '../../../utils/db';
+import Location from '../../../models/Location';
+
+const handler = nc();
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const locations = await Location.find({});
+  db.disconnect();
+  res.send(locations);
+});
+
+export default handler;
