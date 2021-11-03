9f1bdc7e97fe6588175e9f4cf76b4731134a3493
Juan <pront23@gmail.com>
2021-11-03 15:14:55 +0900

    updated location model

---
+++ models/Location.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+
+const locationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    neighborhood: {
+      type: String,
+      required: true,
+    },
+    prefecture: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    cc: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Location =
+  mongoose.models.Location || mongoose.model('Location', locationSchema);
+
+export default Location;
