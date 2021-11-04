3e6297343e7751cb51d723addb6ed6318e8b6421
Juan <pront23@gmail.com>
2021-11-04 13:48:31 +0900

    added animated spinner

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Banner.module.css';
 
-export default function Banner(props) {
+export default function Banner({ buttonText, handleOnClick, isLocating }) {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -10,8 +10,30 @@ export default function Banner(props) {
       </h1>
       <p className={styles.subtitle}>Discover your local coffee shops!</p>
       <div className={styles.btnWrapper}>
-        <button className={styles.btn} onClick={props.handleOnClick}>
-          {props.buttonText}
+        <button className={styles.btn} onClick={handleOnClick}>
+          {isLocating && (
+            <svg
+              className={styles.animateBtn}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className={styles.circle1}
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className={styles.path1}
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          )}
+          <span>{buttonText}</span>
         </button>
       </div>
     </div>
