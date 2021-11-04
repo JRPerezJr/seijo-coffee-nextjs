f0b185dcd04af21db1a75077548ba0091ce8d6b0
Juan <pront23@gmail.com>
2021-11-04 14:03:49 +0900

    removed unused functions and imports

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,12 @@ import useTrackLocation from '../hooks/use-track-location';
 import Head from 'next/head';
 import Image from 'next/image';
 
-// import coffeeStoreData from '../data/coffee-stores.json';
+import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 import Banner from '../components/banner/banner';
 import Card from '../components/card/card';
 
 import styles from '../styles/Home.module.css';
-
-import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 // called on the server
 export async function getStaticProps() {
@@ -17,8 +15,6 @@ export async function getStaticProps() {
 
   return {
     props: {
-      // coffeeStores: coffeeStoreData,
-      // coffeeStores
       coffeeStores,
     },
   };
@@ -49,12 +45,16 @@ export default function Home(props) {
           buttonText="View nearby stores"
           handleOnClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg && (
+          <p>Oops, something when wrong: {locationErrorMsg}</p>
+        )}
+
         <div className={styles.heroImage}>
           <Image src="/static/coffee.jpg" width={400} height={280} />
         </div>
 
         {props.coffeeStores.length > 0 && (
-          <>
+          <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Seijo stores</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((coffeeStore) => {
@@ -69,11 +69,11 @@ export default function Home(props) {
                 );
               })}
             </div>
-          </>
+          </div>
         )}
       </main>
 
-      <footer className={styles.footer}>Powered by Deez Nutz</footer>
+      <footer className={styles.footer}>Powered by Caffeine</footer>
     </div>
   );
 }
