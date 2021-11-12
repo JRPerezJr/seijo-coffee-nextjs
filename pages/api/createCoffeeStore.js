a79ff69a13d546db0599161520fc76580a3c1eb5
Juan <pront23@gmail.com>
2021-11-12 10:05:52 +0900

    changed id to string

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -22,7 +22,7 @@ const createCoffeeStore = async (req, res) => {
         // find a record
         const findCoffeeStoreRecords = await table
           .select({
-            filterByFormula: `id=${id}`,
+            filterByFormula: `id="${id}"`,
           })
           .firstPage();
 
